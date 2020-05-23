10847a7f13e72362e84281c37f141ed31da990ad
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2020-05-23 11:14:54 +0000

    change

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,29 +3,29 @@ import right from './assets/icon-next.svg';
 import left from './assets/icon-prev.svg';
 
 export function Carousel() {
-  const quotes = {
-    0: {
+  const quotes = [
+    {
       image: `${require('./assets/image-tanya.jpg')}`,
       quote:
         '"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."',
       name: 'Tanya Sinclair',
       profession: 'UX Engineer',
     },
-    1: {
+    {
       image: `${require('./assets/image-john.jpg')}`,
       quote:
         '"If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."',
       name: 'John Tarkpor',
       profession: 'Junior Front-end Developer',
     },
-  };
+  ];
 
   const sliderReducer = (state, action) => {
     switch (action.type) {
       case 'NEXT':
-        return quotes[0];
-      case 'PREV':
         return quotes[1];
+      case 'PREV':
+        return quotes[0];
       default:
         return quotes[0];
     }
@@ -40,13 +40,13 @@ export function Carousel() {
           <img src={state.image} alt={state.profession} />
           <div className='btn'>
             <button
-              onClick={() => dispatch({ type: 'NEXT' })}
+              onClick={() => dispatch({ type: 'PREV' })}
               className='btn-left'
             >
               <img src={left} alt='arrow icon' />
             </button>
             <button
-              onClick={() => dispatch({ type: 'PREV' })}
+              onClick={() => dispatch({ type: 'NEXT' })}
               className='btn-rigth'
             >
               <img src={right} alt='arrow icon' />
